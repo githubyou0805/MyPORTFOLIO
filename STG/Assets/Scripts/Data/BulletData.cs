01245ea00a39f7aa195dc065f6244678f8e1782a
githubyou0805 <satokoujirou0805@gmail.com>
2026-09-10 14:15:34 +0900

    Add CatchBall

--- STG/Assets/Scripts/Data/BulletData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "GameData/Bullet")]
-public class BulletData : ScriptableObject
-{
-    public float speed = 10f;
-    public int damage = 1;
-    public int defaultDamage = 1;
-}
--- STG/Assets/Scripts/Data/UIData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "GameData/UI")]
-public class UIData : ScriptableObject
-{
-    public string scorePrefix = "Score: ";
-    public string hpPrefix = "HP: ";
-    public string gameOverText = "GAME OVER";
-}
